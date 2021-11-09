4448721d1a9d71b2b54640d94ebff162cacb4a25
thienlongtran <tltran5@uno.edu>
2021-11-09 16:08:29 -0600

    Fixed s3 deletion

--- infrastructure/s3_input.py
+++ infrastructure/s3_input.py
@@ -1,26 +1,33 @@
 import boto3
 
-client = boto3.client('s3')
-
 class s3_input_bucket:
     def __init__(self, bucket_name):
         self.bucket_name = bucket_name
+        self.client = boto3.client('s3')
     
     def create_bucket(self):
-        response = client.create_bucket(
+        response = self.client.create_bucket(
             Bucket = self.bucket_name
         )
         print(response)
         return response
     
     def delete_bucket(self):
-        response =  client.delete_bucket(
+        self.clear_bucket()
+        response =  self.client.delete_bucket(
             Bucket = self.bucket_name
         )
         print(response)
         return response
 
-    def printBucketName(self):
+    #Citation: learned how to clear s3 bucket with boto3 from Stack Overflow
+    #Link: https://stackoverflow.com/questions/43326493/what-is-the-fastest-way-to-empty-s3-bucket-using-boto3/43328646
+    def clear_bucket(self):
+        s3 = boto3.resource("s3")
+        bucket = s3.Bucket(self.bucket_name)
+        bucket.objects.all().delete()
+
+    def print_bucket_name(self):
         print(self.bucket_name)
 
 newbucket = s3_input_bucket("talia-4452-f21-thien-upload")
