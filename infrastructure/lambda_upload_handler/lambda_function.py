10fb4411d2f0271c795ddef4aa700116c10a26b6
thienlongtran <tltran5@uno.edu>
2021-11-11 22:57:17 -0600

    Update upload handler code

--- infrastructure/lambda_upload_handler/lambda_function.py
+++ infrastructure/lambda_upload_handler/lambda_function.py
@@ -3,14 +3,20 @@ import json
 
 def lambda_handler(event, context):
     
-    client = boto3.client("s3")
+    s3_client = boto3.client("s3")
     bucket_event = event["Records"][0]["s3"]["bucket"]["name"]
     object_name = event["Records"][0]["s3"]["object"]["key"]
     
+    sns_client = boto3.client("sns")
+    
     #Citation: learned how to read file line-by-line from Stack Overflow
     #Link: https://stackoverflow.com/questions/62699659/reading-file-line-by-line-from-s3-on-lambda-trigger
-    for line in client.get_object(Bucket = bucket_event, Key = object_name)["Body"].read().split(b'\n'):
+    for line in s3_client.get_object(Bucket = bucket_event, Key = object_name)["Body"].read().split(b'\n'):
         print(line)
+        response = sns_client.publish(
+                                     TopicArn = "arn:aws:sns:us-east-1:176966333216:nyssa-4452-f21-sns-middle-man",
+                                     Message = str(line)
+                                     )
 
     return {
         'statusCode': 200,
