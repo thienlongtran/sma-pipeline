7e45aecbfaf5615adec8ec6b3ef2e35152b4ecc6
thienlongtran <tltran5@uno.edu>
2021-11-11 22:37:24 -0600

    Updated lambda upload handler function

--- infrastructure/lambda_upload_handler/lambda_function.py
+++ infrastructure/lambda_upload_handler/lambda_function.py
@@ -7,7 +7,9 @@ def lambda_handler(event, context):
     bucket_event = event["Records"][0]["s3"]["bucket"]["name"]
     object_name = event["Records"][0]["s3"]["object"]["key"]
     
-    for line in client.get_object(Bucket = bucket_event, Key = object_name):
+    #Citation: learned how to read file line-by-line from Stack Overflow
+    #Link: https://stackoverflow.com/questions/62699659/reading-file-line-by-line-from-s3-on-lambda-trigger
+    for line in client.get_object(Bucket = bucket_event, Key = object_name)["Body"].read().split(b'\n'):
         print(line)
 
     return {
