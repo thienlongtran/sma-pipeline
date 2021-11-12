5b07b49f3e7603f0d3ab21272894c7f3c3eed92a
thienlongtran <tltran5@uno.edu>
2021-11-11 21:34:51 -0600

    Updated lambda function to read lines in S3 object triggering event

--- infrastructure/lambda_upload_handler/lambda_function.py
+++ infrastructure/lambda_upload_handler/lambda_function.py
@@ -1,7 +1,15 @@
+import boto3
 import json
 
 def lambda_handler(event, context):
-    # TODO implement
+    
+    client = boto3.client("s3")
+    bucket_event = event["Records"][0]["s3"]["bucket"]["name"]
+    object_name = event["Records"][0]["s3"]["object"]["key"]
+    
+    for line in client.get_object(Bucket = bucket_event, Key = object_name):
+        print(line)
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
