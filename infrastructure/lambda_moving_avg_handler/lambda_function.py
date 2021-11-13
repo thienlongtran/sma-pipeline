5758a7653f7f094ea6e8c6c0f45183294a277148
thienlongtran <tltran5@uno.edu>
2021-11-13 16:52:59 -0600

    Update lambda codes

--- infrastructure/lambda_moving_avg_handler/lambda_function.py
+++ infrastructure/lambda_moving_avg_handler/lambda_function.py
@@ -1,16 +1,26 @@
 import boto3
 import json
-import re
+import time
 
 def lambda_handler(event, context):
     message = event["Records"][0]["Sns"]["Message"]
     clean_message = message.split("b'")[1].split("\\r'")[0]
     message_array = clean_message.split(",")
+    message_length = len(message_array)
     
-    print("Message: " + clean_message)
-    print(message_array)
-    print("Message length: " + str(len(message_array)))
-    
+    #Ending Line
+    if message_length == 3:
+        time.sleep(10)
+        print("EOF")
+        print("Message length: " + str(message_length))
+        print(message_array)
+        
+    #Regular Data Line
+    elif message_length == 10:
+        print("Message length: " + str(message_length))
+        print(message_array)
+        
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
--- infrastructure/lambda_upload_handler/lambda_function.py
+++ infrastructure/lambda_upload_handler/lambda_function.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+import time
 
 def lambda_handler(event, context):
     
@@ -11,11 +12,18 @@ def lambda_handler(event, context):
     #Citation: learned how to read file line-by-line from Stack Overflow
     #Link: https://stackoverflow.com/questions/62699659/reading-file-line-by-line-from-s3-on-lambda-trigger
     for line in s3_client.get_object(Bucket = bucket_event, Key = object_name)["Body"].read().split(b'\n'):
+        
+        #Sleep for 10 seconds before sending on final line
+        if len(str(line).split(",")) == 3:
+            print("Sleeping for 10 seconds before sending final line...")
+            time.sleep(10)
+            print("Sending final line...")
+    
         response = sns_client.publish(
                                      TopicArn = "arn:aws:sns:us-east-1:176966333216:nyssa-4452-f21-thien-sns-sync",
                                      Message = str(line)
                                      )
-                                     
+                
     print("All lines successfully sent to SNS.")
 
     return {
