aaa7f5e62d45a1c55876672585e0d02a08c23c3a
thienlongtran <tltran5@uno.edu>
2021-11-12 19:26:05 -0600

    Adding message cleaning to Lambda Moving Avg Handler

--- infrastructure/lambda_moving_avg_handler/lambda_function.py
+++ infrastructure/lambda_moving_avg_handler/lambda_function.py
@@ -1,11 +1,16 @@
 import boto3
 import json
+import re
 
 def lambda_handler(event, context):
     message = event["Records"][0]["Sns"]["Message"]
+    clean_message = message.split("b'")[1].split("\\r'")[0]
+    message_array = clean_message.split(",")
     
-    print(message)
-
+    print("Message: " + clean_message)
+    print(message_array)
+    print("Message length: " + str(len(message_array)))
+    
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
